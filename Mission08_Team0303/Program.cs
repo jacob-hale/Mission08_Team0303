6c022e4bc5a37a1fad1e4fafddcf088bfb4b5df8
Jacob Hale <Jchale23@byui.edu>
2026-02-26 21:56:39 -0700

    finishing touches

--- Mission08_Team0303/Program.cs
+++ Mission08_Team0303/Program.cs
@@ -45,6 +45,6 @@ app.UseAuthorization();
 // -------------------------------------------------------
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Task}/{action=Index}/{id?}");
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
